66189c0a8f9a86f8ce2d23ed7678b331b08d95fc
Rashmi Kadam <rpkadam@ncsu.edu>
2021-08-01 22:30:45 -0400

    Data Exploration added

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ library(shiny)
 library(tidyverse)
 library(kableExtra)
 library(DT)
-
+library(plotly)
 
 
 # Define server logic required to draw a histogram
@@ -37,21 +37,103 @@ shinyServer(function(input, output,session) {
        }
      )
   
+  output$plot <- renderPlotly({
+    #get filtered data
+    if(input$plots =="cat"){
+      if(input$catGraph =="Bar Plot"){
+        ggplotly(ggplot(churnData, aes(fill=Churn, x= !!sym(input$catVar))) + 
+                   geom_bar(position="dodge", stat="count")) 
+        
+      }
+      else {
+        ggplotly(ggplot(churnData, aes(x =!!sym(input$catVar),y=TotalCharges,fill=Churn)) +
+          geom_boxplot() +
+          facet_grid(.~Churn) )
+      }
+    } else {
+      if(input$numGraph =="Density"){
+        ggplotly( ggplot(churnData, aes(!!sym(input$num), fill = Churn)) + geom_density(alpha = 0.2))
+      }
+      else {
+        ggplotly(ggplot(churnData, aes(x=!!sym(input$num), color=Churn)) +
+                   geom_histogram(fill="white", alpha=0.5, position="identity") )
+      }  
+    }
+  })
+  
+
+  output$summaryName <- renderText({
+    if(input$plots=="cat"){
+      return(paste0("Categorical Summary For: ",input$catVar," VS Churn"))
+    } else {
+      return(paste0("Numerical Summary For: ",input$num," VS Churn"))
+    }
+    
+    
+  })
+    
+  
+  
+  output$summaryTable <- DT::renderDataTable({
+    #get filtered data
+    if(input$plots =="cat"){
+          my_tbl = table(churnData %>% select(Churn,!!sym(input$catVar)))
+          printed_tbl = as.data.frame.matrix(my_tbl)
+          printed_tbl
+    } else {
+      
+      statsChurn <- churnData %>%
+        group_by(Churn) %>%
+        summarise(
+          Min = min(!!sym(input$num)),
+          Max = max(!!sym(input$num)),
+          Median = median(!!sym(input$num)),
+          Stdev = sd(!!sym(input$num)),
+          Mean = mean(!!sym(input$num)))
+      statsChurn
+      
+    }
+       
+    },options = list(dom = 't'))
+  
+  
+  # Function to get the summary data
+  
+  
+  
+  # 
   
   
   
   
   
   
+  
+  
+  
+  
+  
+  
+  #getSummaryData <- reactive({
+  #  if(input$cat=="cat"){
+  #    statsAtemp <- churnData %>%
+  #      group_by(Churn) %>%
+  #      summarise(
+  #        atemp.min = min(MonthlyCharges),
+  #        atemp.max = max(MonthlyCharges),
+  #        atemp.med = median(MonthlyCharges),
+  #        atemp.stdev = sd(MonthlyCharges),
+  #        atemp.mean = mean(MonthlyCharges))
+  #    
+  #    return(statsAtemp)
+  #  }
+  #  else{
+  #    newData <- churnData %>% filter(InternetService == input$InternetService) %>% select(input$checkGroup) 
+  #    return(newData)
+   # }
+  })
   
   #output$data <- renderUI(source('data.R', local = TRUE))
-  
-    
-    
-    
-    
-    
-    
     
     
     
@@ -61,4 +143,3 @@ shinyServer(function(input, output,session) {
   #    source('ui2.R', local = TRUE))
  # output$test <- renderUI( paste0('test'))
 
-})
--- ui.R
+++ ui.R
@@ -1,6 +1,8 @@
 
 library(shiny)
 library(shinythemes)
+library(plotly)
+
 
 # Main UI page
 ui <- navbarPage(
@@ -11,7 +13,7 @@ ui <- navbarPage(
     # Tab About
     tabPanel(
         "About",
-        fluidPage(uiOutput("about")),
+        fluidPage(uiOutput("about"))
     ),
     
     # Tab Data
@@ -22,38 +24,53 @@ ui <- navbarPage(
                 sidebarPanel(
                     downloadButton('downloadData', 'Download'),
                     h3("Subseting dataset:"),
-        
-
                     selectInput("InternetService", "Choose Internetsevice type", choices=c("All types" ,"DSL", "Fiber optic", "No")),
                     checkboxGroupInput("checkGroup", 
                                        label = ("Columns to select"),
                                        choices = colnames(churnData),
-                                       selected=c('gender','tenure','SeniorCitizen','Partner','Dependents','InternetService','MonthlyCharges','Churn'))
-        
-                ,
+                                       selected=c('gender','tenure','SeniorCitizen','Partner','Dependents','InternetService','MonthlyCharges','Churn')),
                     width = 3
-                    
-                
                 ),
-                
-                
                 mainPanel(
-                    
                     DT::dataTableOutput("table")
                 )
             )
         )
     ),
+    
     #Tab - Data Exploration
     tabPanel(
         "Data Exploration",
-        # App title ----
-        fluidPage(uiOutput("dataexp"))
+        fluidPage(
+            sidebarLayout(
+                sidebarPanel(
+                    radioButtons("plots", "Data type:",
+                                 c("Categorical" = "cat",
+                                   "Numeric" = "num")),
+                    conditionalPanel(condition = "input.plots=='cat'",
+                                     selectInput("catVar", "Choose Categorical Variable", choices=c("InternetService" ,"PaymentMethod", "Contract")),
+                                        selectInput("catGraph", "Choose Plot Type", choices=c("Bar Plot" ,"Box plot"))),
+                    conditionalPanel(condition = "input.plots=='num'",
+                        selectInput("num", "Choose Numberic Variable", choices=c("Tenure"="tenure" ,"TotalCharges", "MonthlyCharges")),
+                        selectInput("numGraph", "Choose Plot Type", choices=c("Density" ,"Histogram")))
+                    
+                    , width = 3
+                ),
+                mainPanel(
+                    h3("Plot:"),
+                    plotlyOutput("plot"),
+                    hr(style="1px solid #000000"),
+                    h3("Summary Table:"),
+                    tags$b(textOutput("summaryName")),
+                    DT::dataTableOutput("summaryTable")
+                )
+            )
+        )
     ),
     #tab Modeling
     tabPanel(
         "Modeling",
-        fluidPage(uiOutput("modeling")),
+        fluidPage(uiOutput("modeling"))
     )
-)
+  )
         
